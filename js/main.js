4a22596980ea9ba87802f0438462e264b7938d63
dogmax296 <ivanov1maxim@gmail.com>
2015-12-04 15:58:40 +0200

    Finished:
     - second screen
     - footer;
    
    Start:
    modal form;

--- js/main.js
+++ js/main.js
@@ -31,6 +31,26 @@ $(document).ready(function(){
     });
 });
 
-            
+
+// DATEPICKER
+
+$(function() {
+    $( "#from" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function( selectedDate ) {
+        $( "#to" ).datepicker( "option", "minDate", selectedDate );
+      }
+    });
+    $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function( selectedDate ) {
+        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+      }
+    });
+  });     
 
         
